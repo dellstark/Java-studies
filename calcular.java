a5f69f706072385a3a8588c5d4d836960ec0cc52
Wendel Santos <67715702+dellstark@users.noreply.github.com>
2024-04-25 01:26:55 -0300

    Create calcular.java

---
+++ calcular.java
@@ -0,0 +1,10 @@
+public class calcular {
+    public static void main(String[] args) {
+        int notaUm = 6;
+        int notaDois = 10;
+        int media = (notaUm + notaDois) / 2;
+
+        System.out.println(media);
+    }
+
+}
