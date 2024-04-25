8314240df014f490ea1f2377e4a8fab09a233006
Wendel Santos <67715702+dellstark@users.noreply.github.com>
2024-04-25 01:40:17 -0300

    Create Operadores.java
    Aprendendo mais sobre concatenação

---
+++ Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nome = "wendel ";
+        String sobrenome = "santos";
+        String juntar = nome + sobrenome;
+
+        System.out.println("Meu nome agora é: " + juntar);
+    }
+}
