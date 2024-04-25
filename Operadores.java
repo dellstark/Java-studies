1e9f65421456db0c5f4659308950aed362d2f939
Wendel Santos <67715702+dellstark@users.noreply.github.com>
2024-04-25 11:09:30 -0300

    Update Operadores.java
    testando novos operadores

--- Operadores.java
+++ Operadores.java
@@ -1,9 +1,20 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nome = "wendel ";
-        String sobrenome = "santos";
-        String juntar = nome + sobrenome;
+        /*
+         * String nome = "wendel ";
+         * String sobrenome = "santos";
+         * String juntar = nome + sobrenome;
+         * 
+         * System.out.println("Meu nome agora é: " + juntar);
+         */
 
-        System.out.println("Meu nome agora é: " + juntar);
+        /* testando operadores +, -, ++, --, ! */
+
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+        /* transforma o numero negativo em positivo novamente */
+        numero = numero * -1;
+        System.out.println(numero);
     }
 }
