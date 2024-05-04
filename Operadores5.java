3ee767ceebf067291b0de3ea6afaf56e1e4a7855
Wendel Santos <67715702+dellstark@users.noreply.github.com>
2024-05-04 01:56:20 -0300

    Create Operadores5.java
    Aprendendo um pouco sobre operadores ternários

---
+++ Operadores5.java
@@ -0,0 +1,10 @@
+public class Operadores5 {
+    public static void main(String[] args) {
+        var a = 6;
+        var b = 5;
+        String resultado = a == b ? "verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+
+    }
+}
